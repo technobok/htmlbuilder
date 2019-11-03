302acaf0bb545883b92996c8fd20f193e962773c
pawe <pawe@localhost.localdomain>
2019-11-03 12:23:12 +1000

    Fix license and setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,20 @@
-# TODO:
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='htmlbuilder',
+    version='0.1.0',
+    description='htmlbuilder module',
+    long_description=readme,
+    author='Paul Webb',
+    author_email='p@technobok.org',
+    url='https://dev.technobok.org/technobok/htmlbuilder',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
 
